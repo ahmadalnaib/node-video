efc83df4a34de8ab8277a675d5bd276169ad9f3f
ahmedhabeeb73 <az73system@gmail.com>
2019-11-19 19:06:43 +0100

    add nodemon and simple router

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ const express= require("express");
 const app= express();
 
 
+// index route
+app.get('/', (req, res) => {
+  res.send("home");
+  
+});
+// about 
+app.get('/about', (req, res) => {
+  res.send("about");
+  
+});
+
+
 const Port=3000;
 
 app.listen(Port, () => {
